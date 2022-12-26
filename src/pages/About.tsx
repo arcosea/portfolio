fcc0856f592f683f292496ba2f3bb657c1a9483b
Erick Arcos <arcosea@plu.edu>
2022-12-25 17:10:19 -0800

    content on about me

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,22 +1,56 @@
 import "../styling/About.css";
 import {Link} from "react-router-dom";
+import React, { useEffect, useState, useRef } from 'react';
+import ImageSlider, { Slide } from "react-auto-image-slider";
 
 export function About(){
+    
+
+
     return (
         <div id="container">
             <h2 id="header"> About Me</h2>
 
             <div id="person">
                 <p id = "info">
-                    Hey Everyone! My Name is <strong>Erick Arcos</strong>, a student at <em>Pacific Lutheran University</em>. 
+                    Hey Everyone! My Name is <strong>Erick Arcos</strong>, a student-athlete at <em>Pacific Lutheran University</em>. 
                     I am studying <strong>Applied Physics (BS)</strong> and <strong>Computer Science (BA)</strong> with a Minor in Mathematics. 
-                    I have interests in robotics, embedded systems, aerospace engineering, Artificial Intelligence,
+                    I have interests in robotics, Space exploration, Astronomy, aerospace engineering, embedded systems, Artificial Intelligence,
                     Machine learning, software development and so much more!
                 </p>
                 <img src="/profile.png" id="profile" alt="profile picture of Erick Arcos"></img>
-    
             </div>
-        
+
+            <hr></hr>
+            <section>
+                <h3 id="sub-header"> Family & Background </h3>
+                <p>
+                    Family & Friends and the time I spend with them is something I value dearly. I am the eldest of a total of four boys. I am the first in my family to have graduated
+                    High school and attended University! My parents and I were born in Mexico. However, unlike them, I was brought to the United States at a young age
+                    and did not have the pressure to work from an early age to help provide essentials. I had the opportunity and support to be able to attend higher education. 
+                </p>
+            </section>
+            <section>
+                <h3 id="sub-header"> Sports & Hobbies </h3>
+                <p>
+                    Whether it was street soccer or club, I had always enjoyed playing Soccer. While I had played street soccer in the early stages of my life, I didn't play officially for a team
+                    till 6th grade. Aside from Soccer, I also really enjoyed Running. I had run in Track for the first time in 8th grade where I ran a 5:24 mile. After a poor first and second year soccer experience 
+                    in High School, I eventually decided to stop playing soccer & focus mainly on Track and Cross Country. I ended High School with a 4:45 mile and a 18:09 5Km personal Best.
+                </p>
+                <p>
+                    I attended Pacific Lutheran University where I continued running Track and Cross Country. My Freshman year I only ran Track where I finished the season with
+                    a 16:35 5Km back in 2019. While, my collegiate running career comes to an end this Spring of 2023, I have since then ran a 
+                    15:43 5Km in Track and 26:17 8Km personal bests. I have also done an unofficial half marathon in 1:18:05.
+                </p>
+            </section>
+
+            <section>
+                <h3 id="sub-header"> Favorite Shows & Movies </h3>
+                <p>
+                    Here are a few slide shows
+                </p>
+                
+            </section>
         
         
         
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,13 @@
 import "../styling/Home.css";
 import {Link} from "react-router-dom";
 import React, { useEffect, useState, useRef } from 'react';
+import * as THREE from "three";
+import TextGeometry, { BMFont, BMFontJsonParser, TextGeometryOption, TextAlign } from 'three-text-geometry';
+import { TTFLoader } from 'three/examples/jsm/loaders/TTFLoader';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { createScanner, isWhiteSpaceLike } from "typescript";
+import { BooleanKeyframeTrack } from "three";
+
 
 export function Home(){
     const [name, setName] = useState("");
@@ -23,8 +30,56 @@ export function Home(){
         setMessage(value)
     }
 
+    // Welcome page
+    useEffect(() => {
+        let scene: THREE.Scene;
+        let camera: THREE.PerspectiveCamera;
+        let renderer: THREE.WebGLRenderer;
+        let cube = new THREE.Mesh();
+
+        function init() {
+            // Create scene object
+            scene = new THREE.Scene();
+            scene.background = new THREE.Color("rgb(245, 245, 220)")
+
+            // Setup camera with facing upward
+            camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight);
+            camera.position.z = 40;
+
+            // Setup renderer
+            renderer = new THREE.WebGLRenderer();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+
+            window.addEventListener("resize", (event) => {
+                renderer.setSize(window.innerWidth, window.innerHeight);
+            })
+            
+            // Add renderer to only this page
+            document.querySelector(".home")?.appendChild(renderer.domElement)
+        
+            // Add 
+            const geometry = new THREE.BoxGeometry( 20, 20, 20 );
+            const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+            cube = new THREE.Mesh( geometry, material );
+            scene.add( cube );
+
+
+            animate();
+        }
+        // Rendering loop
+        function animate() {
+            cube.rotateX(0.01);
+            cube.rotateY(0.01);
+            renderer.render(scene, camera);
+            requestAnimationFrame(animate);
+        }
+
+        init();
+    }, []);
+
+
     return (
-        <div id="container">
+        <div className="home">
             <h1 > Welcome to my Page!</h1>
             <div id="3js">
 
