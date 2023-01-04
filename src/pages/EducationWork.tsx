fa2e821265a831dd74119074908740fe544b7666
Erick Arcos <arcosea@plu.edu>
2023-01-03 21:39:41 -0800

    styling work page

--- src/pages/EducationWork.tsx
+++ src/pages/EducationWork.tsx
@@ -25,6 +25,9 @@ export function EducationWork(){
             observer.observe(elem);
         })
     }, []);
+
+    const NNGif = "/design/nn.gif";
+    const mathGif = "/design/math.gif";
     return (
         <div className="container">
             <div className="section-header">
@@ -32,74 +35,92 @@ export function EducationWork(){
             </div>
             <section className="section" id="work-section-timeline">
                 {/* SWE Job */}
-                <div className="comp-container-left hidden" id="swe-job">
-                    <h3 className="component-name"> Software Engineering Intern</h3>
-                    <img src="logos/naimor.png" alt="company logo" id="naimor-img"></img>
-                    <span className="left-arrow"></span>
-                    <h4 className="company-left"> NaiMor, Inc </h4>
-                    <span className="timeline"> August. 2022 - Present</span>
-                    <p className="description">
-                        Contracted to create a UI to embed into <strong> Shopify </strong> that allows clients to customize a product based on different <strong> 
-                        styles, materials, shape, color, preferred dimensions and other features </strong>. The website assembles and renders
-                        the product using <em> Javascripts 3D Library (3JS) </em> & associates a <strong> price </strong> for the current assembly. 
-                        This project is done in collaborate with another intern under the guidance of a mentor.
-                    </p>
-                    <p className="note">
-                        Will provide <strong><em>link</em></strong> to website once its complete.
-                    </p>
-                    
+                <div className="inner-container">
+                    <div className="comp-container-left hidden" id="swe-job">
+                        <h3 className="component-name"> Software Engineering Intern</h3>
+                        <img src="logos/naimor.png" alt="company logo" id="naimor-img"></img>
+                        <span className="left-arrow"></span>
+                        <h4 className="company-left" id="naimor"> NaiMor, Inc </h4>
+                        <span className="timeline"> August. 2022 - Present</span>
+                        <p className="description">
+                            Contracted to create a UI to embed into <strong id="shopify"> Shopify </strong > that allows clients to customize a product based on different <strong id="parameters"> 
+                            styles, materials, shape, color, preferred dimensions and other features. </strong> 
+                        </p>
+                        <p className="description">
+                        The website assembles and renders
+                            the product using <em id="threejs"> Javascripts 3D Library (3JS) </em> & associates a <strong> price </strong> for the current assembly. 
+                            This project is done in collaborate with another intern under the guidance of a mentor.
+                        </p>
+                        <p className="note">
+                            Will provide <strong><em>link</em></strong> to website once its complete.
+                        </p>  
+                    </div>
+                    <div className="visual">
+                    </div>
                 </div>
 
                 {/* Tutor Job */}
-                <div className="comp-container-right hidden" id="tutor-job">
-                    <h3 className="component-name"> Math Tutor</h3>
-                    <img src="logos/plu2.png" alt="company logo"></img>
-                    <span className="right-arrow"></span>
-                    <h4 className="company-right"> Pacific Lutheran University </h4>
-                    <span className="timeline"> August 2022 - Present</span>
-                    <p className="description">
-                        I am responsible for providing academic assistance to other students through drop-in hours or scheduled one to one appointments for a total
-                        of about 6-7 hours per week. Some of the classes I tutor for include: <strong>Alegbra, Pre-calculus, Calculus I, II and III, as well as Linear Algebra
-                        and Differential Equations </strong>.
-                    </p> 
+                <div className="inner-container">
+                    <div className="visual">
+                    </div>
+                    <div className="comp-container-right hidden" id="tutor-job">
+                        <h3 className="component-name"> Math Tutor</h3>
+                        <img src="logos/plu2.png" alt="company logo"></img>
+                        <span className="right-arrow"></span>
+                        <h4 className="company-right" id="plu"> Pacific Lutheran University </h4>
+                        <span className="timeline"> August 2022 - Present</span>
+                        <p className="description">
+                            Responsible for providing <strong id="ac-sys"> academic assistance </strong> to other students through drop-in hours or scheduled one to one appointments.
+                        </p> 
+                    </div>
                 </div>
 
                 {/* TA Job */}
-                <div className="comp-container-left hidden" id="ta-job">
-                    <h3 className="component-name"> Data Structures Teacher Assistant</h3>
-                    <img src="logos/plu.png" alt="company logo"></img>
-                    <span className="left-arrow"></span>
-                    <h4 className="company-left"> Pacific Lutheran University </h4>
-                    <span className="timeline"> February 2022 - May 2022</span>
-                    <p className="description">
-                        I was responsible for checking students pre-lab responses & answering any technical or conceptual lab related questions students may have had.
-                        Aside from assisting students with their labs every week, I was also responsible for grading labs. I ultimately graded 6 different labs for 
-                        roughly 40 students each & provided constructive feedback.
-                    </p> 
-
+                <div className="inner-container">
+                    <div className="comp-container-left hidden" id="ta-job">
+                        <h3 className="component-name"> Data Structures Teacher Assistant</h3>
+                        <img src="logos/plu.png" alt="company logo"></img>
+                        <span className="left-arrow"></span>
+                        <h4 className="company-left" id="plu"> Pacific Lutheran University </h4>
+                        <span className="timeline"> February 2022 - May 2022</span>
+                        <p className="description">
+                            I was responsible for checking students pre-lab responses & answering any <em id="lab"> technical or conceptual lab related questions </em>students may have had.
+                            Also graded <strong id="num-lab"> 6 different labs </strong> for roughly <strong id="num-lab"> 40 students </strong> each & provided <strong id="cfeed"> constructive feedback</strong>
+                        </p>
+                       
+                    </div>
+                    <div className="visual">
+                        <img className='img-vis'src={NNGif}></img>
+                    </div>
                 </div>
 
                 {/* Apprentice Job */}
-                <div className="comp-container-right hidden" id="app-job">
-                    <h3 className="component-name"> Apprentice</h3>
-                    <img src="logos/utiligi.png" alt="company logo"></img>
-                    <span className="right-arrow"></span>
-                    <h4 className="company-right"> Utiligi Telecommunications </h4>
-                    <span className="timeline"> June - September 2020, 2021, 2022</span>
-                    <p className="description">
-                        I was responsible for assisting my coworkers in various types of jobs. Some of the types of jobs I participated in include:
-                        <strong> Underground OSP Cabling, Horizontal Directional Drilling (HDD), Hydro Excavating, Trenching and Concrete/Asphalt Restoration, HDD Bore Profiling and Planning
-                        , Duct Verification (Rod & Proof), Optic Fiber Installation and Traffic Control</strong>. 
-                    </p> 
-        
-                    <p className="note">
-                        <strong>Certifications Gained: </strong>Washington Flagging Permit, CPR 
-                    </p>
+                <div className="inner-container">
+                    <div className="visual">
+                    </div>
+                    <div className="comp-container-right hidden" id="app-job">
+                        <h3 className="component-name"> Apprentice</h3>
+                        <img src="logos/utiligi.png" alt="company logo"></img>
+                        <span className="right-arrow"></span>
+                        <h4 className="company-right" id="utiligi"> Utiligi Telecommunications </h4>
+                        <span className="timeline"> June - September 2020, 2021, 2022</span>
+                        <p className="description">
+                            I was responsible for assisting my coworkers in various types of jobs. Some of the types of jobs I participated in include:
+                            <strong id="work-type"> Underground OSP Cabling, Horizontal Directional Drilling (HDD), Hydro Excavating, Trenching and Concrete/Asphalt Restoration, HDD Bore Profiling and Planning
+                            , Duct Verification (Rod & Proof), Optic Fiber Installation and Traffic Control</strong>. 
+                        </p> 
+            
+                        <p className="note">
+                            <strong id="certs">Certifications Gained: </strong>Washington Flagging Permit, CPR 
+                        </p>
+                    </div>
                 </div>
             </section>
 
             {/************ Section for Education *************/}
-            <h2 className="section-header"> Education </h2>
+            <div className="section-header">
+                <h2 className="header-name hidden"> Education </h2>
+            </div>
             <section className="section" id="ed-section">
                 {/***  Physics BS  ******/}
                 <div className="comp-container" id="phys-ed">
