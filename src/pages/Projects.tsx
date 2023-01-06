48ef61b07dc0e163097b8900ae10e79513ab7852
Erick Arcos <arcosea@plu.edu>
2023-01-06 14:34:07 -0800

    playing w reveal animation

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,11 +4,43 @@ import "../styling/Projects.css";
 import { SpaceCanvas } from './SpaceCanvas';
 
 export function Projects(){
-  
+     /*
+    *  Constantly create animation
+    */
+     useEffect( () => {
+      // Callback function to see if elements are visible
+      const observer = new IntersectionObserver((entries) => {
+          entries.forEach((entry) => {
+              if(entry.isIntersecting){
+                  entry.target.classList.add("reveal");
+              } else{
+                  entry.target.classList.remove("reveal");
+              }
+          })
+      });
+
+      // Display each element that was originally hidden
+      const hiddenElements = document.querySelectorAll(".hide");
+      hiddenElements.forEach( (elem) =>{
+          observer.observe(elem);
+      })
+  }, []);
+
     return (
       <div className='projects' >
         <SpaceCanvas/>
-        <h2 id="proj-header"> Projects</h2>
+        <h1 id="proj-site-header"> Projects </h1>
+
+        <div className="proj-container hide">
+            <span className='proj-title'> StairMaster</span>
+            <p className='proj-desc'> asadqqwd</p>
+            <span className='proj-note'>site</span>
+        </div>
+        <div className="proj-container hide">
+            <span className='proj-title'> title</span>
+            <p className='proj-desc'> asadqqwd</p>
+            <span className='proj-note'>site</span>
+        </div>
       </div>
 
 
