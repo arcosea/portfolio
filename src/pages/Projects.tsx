2fd2fb6286afec42c76673b902e1fcaa126a787a
Erick Arcos <arcosea@plu.edu>
2023-01-07 18:35:20 -0800

    added rest of eng/phys projects & links to pdfs

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,3 +1,5 @@
+
+// Imports
 import * as THREE from 'three'
 import React, { useEffect, useState, useRef } from 'react';
 import "../styling/Projects.css";
@@ -6,8 +8,10 @@ import { Gi3DStairs} from "react-icons/gi";
 import { AiOutlineCaretRight } from "react-icons/ai";
 import { TbLego} from "react-icons/tb";
 import {RiStackFill} from "react-icons/ri";
-import {BsStars} from "react-icons/bs";
-import {FaJedi} from "react-icons/fa";
+import {BsStars, BsController} from "react-icons/bs";
+import {FaJedi, FaCarSide} from "react-icons/fa";
+import {SiProbot} from "react-icons/si";
+import {GiSpinningBlades, GiSoundWaves} from "react-icons/gi";
 
 /**
  * Class that displays all projects
@@ -32,14 +36,25 @@ export function Projects(){
         const hiddenElements = document.querySelectorAll(".proj-container");
         hiddenElements.forEach( (elem) =>{
             observer.observe(elem);
-        })
+        });
+
+
     }, []);
 
+    // Opening & closing text for each section
     let sweOpen = "< SoftwareProjects >";
     let sweClose = "< SoftwareProjects />";
+    let eOpen = "< EngineeringProjects >";
+    let eClose = "< EngineeringProjects />";
+    let pOpen = "< PhysicsProjects >";
+    let pClose = "< PhysicsProjects />";
 
-    let epOpen = "< EngineeringPhysicsProjects >";
-    let epClose = "< EngineeringPhysicsProjects />";
+    // links to pdfs
+    let plcPDF = "../papers/PropellerLiftCoefficient.pdf"
+    let miPDF = "../papers/MichelsonInterferometer.pdf";
+    let ncPDF = "../papers/NuclearScattering.pdf"
+
+    // HTML project
     return (
       <div className='projects' >
         <SpaceCanvas/>
@@ -90,29 +105,109 @@ export function Projects(){
             <span className='proj-title'> <BsStars/> Star Battle Puzzle</span>
             <p className='proj-desc'> Collaborated with a group to design & implement a UI for a sudoku-like puzzle that users can play. </p>
             <span className='proj-note'>
-                <a target="_blank" id="view-website" href="https://lego-finder-7ed0f.web.app/"> <AiOutlineCaretRight/> View Website</a>
+                <a target="_blank" id="view-website" href=""> <AiOutlineCaretRight/> View Website</a>
             </span>
             <span className='hidden-note' data-msg="Java, Java Swing GUI, HTTP Server/Client, Agile Development, Github">
                 <RiStackFill className='tech-stack-icon'/> Tech Stack
             </span>
         </div>
 
-        {/* End swe projects & start eng/phys projects */}
+        {/* End swe projects & start engineering projects */}
         <h2 className="proj-type-header" id="swe-type">  {sweClose} </h2>
         <div className="proj-gap"></div>
-        <h2 className="proj-type-header" id="ep-type">  {epOpen} </h2>
+        <h2 className="proj-type-header" id="e-type">  {eOpen} </h2>
 
         {/* LaserDrive  */}
-
+        <div className="proj-container" id="even-proj">
+            <span className='proj-title'> <FaCarSide/> LaserDrive </span>
+            <p className='proj-desc'> Collaborating with a group to build an Autonomous F1Tenth RC Car. </p>
+            <span className='proj-note'>
+                <a target="_blank" id="view-website" href=""> <AiOutlineCaretRight/> View Website</a>
+            </span>
+            <span className='hidden-note' data-msg="Python, ROS, F1Tenth Car & Simulator, Asana, React, Github">
+                <RiStackFill className='tech-stack-icon'/> Tech Stack
+            </span>
+        </div>
 
         {/** SmartCar */}
-
+        <div className="proj-container" id="odd-proj">
+            <span className='proj-title'> <SiProbot/> Arduino SmartCar </span>
+            <p className='proj-desc'> Infrared remote (IR) controlled car with obstacle detection, avoidance and a follower modes
+            aside from simple driving commands. Also attached OLED Screen that displays temperature, humidity and time. </p>
+            <span className='proj-note'>
+                <a target="_blank" id="view-website" href=""> <AiOutlineCaretRight/> View Website</a>
+            </span>
+            <span className='hidden-note' data-msg="C/C++, Arduino Uno, Electronic modules (Ultrasonic Sensor, DC Motors, PIR motion sensor, IR controller) ">
+                <RiStackFill className='tech-stack-icon'/> Tech Stack
+            </span>
+        </div>
 
         {/** Arduino Controller  */}
+        <div className="proj-container" id="last-proj">
+            <span className='proj-title'> <BsController/> Arduino Controller </span>
+            <p className='proj-desc'> Built a custom RF transmitter & receiver with nRF24L01 modules to control DC motors using joysticks. 
+                                        Also decoded an IR controller & repurposed it to change colors of an LED strip.
+            </p>
+            <span className='proj-note'>
+                <a target="_blank" id="view-website" href=""> <AiOutlineCaretRight/> View Website</a>
+            </span>
+            <span className='hidden-note' data-msg="C/C++, Arduino Nano, Electronic modules (BLDC Motors, nRF24L01) ">
+                <RiStackFill className='tech-stack-icon'/> Tech Stack
+            </span>
+        </div>
 
-        <h2 className="proj-type-header" id="ep-type">  {epClose} </h2>
+        {/** End Engineering Section & Start Physics sections*/}
+        <h2 className="proj-type-header" id="e-type">  {eClose} </h2>
+        <div className="proj-gap"></div>
+        <h2 className="proj-type-header" id="p-type">  {pOpen} </h2>
 
+        {/** Lift Propulsion  */}
+        <div className="proj-container" >
+            <span className='proj-title'> <GiSpinningBlades/> Lift Propeller Coefficient </span>
+            <p className='proj-desc'> An experimental study & analysis on the lift generated from rotating a 2-bladed propeller.
+            </p>
+            <span className='proj-note'>
+                <a target="_blank" id="view-website" onClick={ () => window.open(plcPDF)}> <AiOutlineCaretRight/> View PDF</a>
+            </span>
+            <span className='hidden-note' data-msg="Michelson Interferometer, Excel, Solver, Laser, Camera">
+                <RiStackFill className='tech-stack-icon'/> Tools
+            </span>
+        </div>
 
+        {/** Michelson Interferometer */}
+        <div className="proj-container" >
+            <span className='proj-title'> <GiSoundWaves/> Michelson interferometer Experiment. </span>
+            <p className='proj-desc'> Observed changes in destructive interference patterns created by
+                    interferometer and determined the wavelength of an HeNe laser to be
+                    within 3.8% of expected value. Also determined the index of refraction for
+                    air.
+            </p>
+            <span className='proj-note'>
+                <a target="_blank" id="view-website" onClick={ () => window.open(miPDF)}> <AiOutlineCaretRight/> View PDF</a>
+            </span>
+            <span className='hidden-note' data-msg="Michelson Interferometer, Excel, Solver, Laser, Camera">
+                <RiStackFill className='tech-stack-icon'/> Tools
+            </span>
+        </div>
+
+        {/** Nuclear Scattering */}
+        <div className="proj-container" id="last-proj">
+            <span className='proj-title'> <GiSoundWaves/> Nuclear Scattering Simulation. </span>
+            <p className='proj-desc'> Performed an analogous experiment where ball bearings were fired at a cylindrical target &
+                    scattering angles were observed. Determined its radius to be within 4.3% of
+                    actual value. Gained experience with uncertainty analysis & writing a
+                    scientific report.
+            </p>
+            <span className='proj-note'>
+                <a target="_blank" id="view-website" onClick={ () => window.open(ncPDF)}> <AiOutlineCaretRight/> View PDF</a>
+            </span>
+            <span className='hidden-note' data-msg="Excel, Solver, Uncertainty Analysis">
+                <RiStackFill className='tech-stack-icon'/> Tools
+            </span>
+        </div>
+        <h2 className="proj-type-header" id="p-type">  {pClose} </h2>
+
+        {/** End */}
       </div>
     ); 
 }
