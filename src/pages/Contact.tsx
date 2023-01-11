276b31b136d0832220c256576f4003ea7bfccee2
Erick Arcos <arcosea@plu.edu>
2023-01-11 14:14:19 -0800

    styled contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,17 +1,20 @@
 import "../styling/Contact.css";
-import {Link} from "react-router-dom";
-import {useRef, useState} from "react";
 import { Form, Input, TextArea, Button } from "semantic-ui-react";
 import emailjs from "emailjs-com";
-import Swal from "sweetalert2";
+import { BsPersonBadge } from "react-icons/bs";
+import { MdMessage, MdOutlineMarkEmailUnread, MdSend } from "react-icons/md";
+import { useState } from "react";
 
+// Define ID
+const SERVICE_ID = 'service_rxcozvi';
+const TEMPLATE_ID = 'template_x9asmo8';
+const USER_ID = 'N-Co5OaAcmUr9I2u-';
 
 export function Contact(){
-    // Define ID
-    const SERVICE_ID = 'service_rxcozvi';
-    const TEMPLATE_ID = 'template_x9asmo8';
-    const USER_ID = 'template_x9asmo8';
+    // Keep track of whether email was sent
+    let [sent, setSent] = useState(false);
 
+    // Function to send emails
     const sendEmail = (e: any) => {
         e.preventDefault();
 
@@ -24,20 +27,30 @@ export function Contact(){
         e.target.reset();
     };
 
+
+    
+
     return (
         <div className="cont-container">
             <h2 className="cont-header"> Contact Me</h2>
-            <Form onSubmit={sendEmail}>
-                <label>Name</label>
-                <input type="text" name="name" />
+            <Form className="form-container" onSubmit={sendEmail}>
+                <div className="ind-form-cont">
+                    <label className="form-name"> <BsPersonBadge/> Name: </label>
+                    <input required className="form-input" type="text" name="name" />
+                </div>
+                <div className="ind-form-cont">
+                    <label className="form-name"> <MdOutlineMarkEmailUnread/> Email: </label>
+                    <input required className="form-input" type="email" name="email" />
+                </div>
+                <div className="ind-form-cont">
+                    <label className="form-name"> <MdMessage/> Message: </label>
+                    <textarea className="form-input" name="message" />
+                </div>
+                <div className="ind-form-cont">
+                    <input className="submit" type="submit" value="Submit"/>
+                </div>
                 
-                <label>Email</label>
-                <input type="email" name="email" />
                 
-                <label>Message</label>
-                <textarea name="message" />
-                
-                <input type="submit" value="Send" />
             </Form>
 
         </div>
