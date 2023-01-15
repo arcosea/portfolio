aa892bde82920f5980a27d93c9df0a28d85bb49c
Erick Arcos <arcosea@plu.edu>
2023-01-14 18:02:19 -0800

    icon for home page navbar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,14 +1,12 @@
+import { FaHome } from "react-icons/fa";
 import {Link} from "react-router-dom"
 import { isPropertySignature } from "typescript";
 
 export default function Navbar(){
     return (
         <nav className="nav">
-            <Link to="/" className="site-title"> Welcome</Link>
+            <Link to="/" className="site-title"> <FaHome/> Home </Link>
             <ul>
-                <li> 
-                    <Link to="/" className="site-title"> Home</Link>
-                </li>
                 <li>
                     <Link to="/experience" className="site-title"> Experience</Link>
                 </li>
